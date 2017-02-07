362f396847a1658f3bab14a6a12c48958cc16d41
Eric Halsey <eric.halsey@pimco.com>
2017-02-07 14:22:24 -0800

    not working need return values

---
+++ ex5/ex6.js
@@ -0,0 +1,36 @@
+﻿(function main(global) {
+    var f1results = '';
+    var f2results = '';
+    var f3results = '';
+    $.when(getFile("file1", f1results), getFile("file2", f2results), getFile("file3", f3results)).then(function () {
+        console.log(f1results);
+        console.log(f2results);
+        console.log(f3results);
+        console.log("complete!");
+    });
+})(window);
+
+function getFile(file,results) {
+    var deferred = jQuery.Deferred();
+    fakeAjax(file, function (text) {
+        deferred.resolve();
+        results = text;
+    });
+    return deferred.promise();
+}
+
+function fakeAjax(url, cb) {
+    var fake_responses = {
+        "file1": "The first text",
+        "file2": "The middle text",
+        "file3": "The last text"
+    };
+    var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+    console.log("Requesting: " + url);
+
+    setTimeout(function () {
+        cb(fake_responses[url]);
+    }, randomDelay);
+}
+
