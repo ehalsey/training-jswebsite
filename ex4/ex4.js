747638628ea15b88cc927fa9188a87f942dccd70
Eric Halsey <eric.halsey@pimco.com>
2017-02-07 11:06:25 -0800

    init

---
+++ ex4/ex4.js
@@ -0,0 +1,43 @@
+function Widget(width,height) {
+	this.width = width || 50;
+	this.height = height || 50;
+	this.$elem = null;
+}
+
+Widget.prototype.render = function($where){
+	if (this.$elem) {
+		this.$elem.css({
+			width: this.width + "px",
+			height: this.height + "px"
+		}).appendTo($where);
+	}
+};
+
+function Button(width, height, label) {
+    Widget.call(this, width, height);
+    this.label = label;
+    this.$elem = $("<button>").text(this.label);
+}
+
+Button.prototype = Object.create(Widget.prototype);
+
+Button.prototype.render = function ($where) {
+    var self = this;
+    this.__proto__.__proto__.render($where).bind(self);
+    this.$elem.on('click', function configHandle(evt) {
+        this.$elem.onClick(evt);
+    });
+}
+
+Button.onClick = function (evt) {
+    console.log('Button ' + evt.target.text + 'clicked!');
+}
+
+$(document).ready(function(){
+    var $body = $(document.body);
+    var btn1 = new Button(100, 100, "button 1");
+    //var btn2 = new Button(100, 100, "button 2");
+
+    btn1.render($body);
+    //btn2.render($body);
+});
